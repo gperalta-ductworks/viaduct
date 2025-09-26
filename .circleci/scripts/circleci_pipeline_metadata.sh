55710840a79f5b0c5418446903a5dfd0f679d3e5
Gabriel Peralta <gabriel@ductworks.io>
2025-09-26 10:55:35 -0300

    Update circleci_pipeline_metadata.sh

--- .circleci/scripts/circleci_pipeline_metadata.sh
+++ .circleci/scripts/circleci_pipeline_metadata.sh
@@ -2,7 +2,6 @@ set -euo pipefail
 sudo apt-get update -y && sudo apt-get install -y jq
 
 VCS="github"
-
 PIPE_NUM="$(curl -sS -H "Circle-Token: ${CIRCLE_TOKEN}" "https://circleci.com/api/v2/pipeline/${CIRCLE_PIPELINE_ID}" | jq -r '.number')"
 PIPELINE_NAME="$(curl -sS -H "Circle-Token: ${CIRCLE_TOKEN}" "https://circleci.com/api/v2/pipeline/${CIRCLE_PIPELINE_ID}/workflow" | jq -r '.items[0].name')"
 PIPELINE_URL="https://app.circleci.com/pipelines/${VCS}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/${PIPE_NUM}"
@@ -10,7 +9,7 @@ WORKFLOW_URL="${PIPELINE_URL}/workflows/${CIRCLE_WORKFLOW_ID}"
 JOB_URL="https://app.circleci.com/pipelines/${VCS}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/${PIPE_NUM}/workflows/${CIRCLE_WORKFLOW_ID}/jobs/${CIRCLE_BUILD_NUM}"
 
 MSG="$(cat <<EOF
------------ Circle-Ci Error: -------------
+----------- Circle-Ci Error -------------
 
 project_name=${CIRCLE_PROJECT_REPONAME}
 org=${CIRCLE_PROJECT_USERNAME}
@@ -28,13 +27,13 @@ job_url=${JOB_URL}
 ------------------------------------------
 EOF
 )"
-# Sustituye los placeholders por sus valores
+#Replace the placeholders with their values
 MSG="${MSG//%PIPELINE_URL%/${PIPELINE_URL}}"
 MSG="${MSG//%WORKFLOW_URL%/${WORKFLOW_URL}}"
 
 
-# Construye JSON con jq; preserva saltos de línea como \n
+#Build JSON with jq; preserve line breaks as \n
 PAYLOAD="$(jq -n --arg t "$MSG" '{text:$t}')"
 
-# Exporta para el orb de Slack
+# Export for the Slack orb
 printf 'export SLACK_PARAM_CUSTOM=%q\n' "$PAYLOAD" >> "$BASH_ENV"
